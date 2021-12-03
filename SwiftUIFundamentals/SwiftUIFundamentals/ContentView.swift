9cb8fda5ff3f12656d4845753e766b6fab9a6d6b
My3 Shenoy <shenoymy31@gmail.com>
2021-12-03 15:22:23 -0500

    Added basic UI state and data binding

--- SwiftUIFundamentals/SwiftUIFundamentals/ContentView.swift
+++ SwiftUIFundamentals/SwiftUIFundamentals/ContentView.swift
@@ -8,32 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showAlert = false
+    @State private var sliderValue = 10.0
+    
     var body: some View {
         VStack {
             VStack {
-                Text("🎯 PUT THE BULLSEYE AS CLOSE AS YOU CAN TO 🎯")
-                    .kerning(2.0)
-                    .bold()
-                    .multilineTextAlignment(.center)
-                    .lineSpacing(4)
-                    .font(.footnote)
-                Text("89")
-                    .bold()
-                    .kerning(-1)
-                    .fontWeight(.black)
-                    .font(.largeTitle)
-                HStack {
-                    Text("1")
+                VStack {
+                    Text("🎯 PUT THE BULLSEYE AS CLOSE AS YOU CAN TO 🎯")
+                        .kerning(2.0)
                         .bold()
-                        .fontWeight(.black)
-                    Slider(value: .constant(50), in: 1.0...100.0)
-                    Text("100")
+                        .multilineTextAlignment(.center)
+                        .lineSpacing(4)
+                        .font(.footnote)
+                    Text("89")
                         .bold()
+                        .kerning(-1)
                         .fontWeight(.black)
+                        .font(.largeTitle)
+                    HStack {
+                        Text("1")
+                            .bold()
+                            .fontWeight(.black)
+                        Slider(value: $sliderValue, in: 1.0...100.0)
+                        Text("100")
+                            .bold()
+                            .fontWeight(.black)
+                    }
                 }
-            }
-            Button(action: {}) {
-                Text("Hit Me")
+                Button(action: {
+                    showAlert = true
+                }) {
+                    Text("Hit Me")
+                }
+                .alert(isPresented: $showAlert) {
+                    let roundedValue = Int(sliderValue.rounded())
+                    return Alert(title: Text("Hello There!"), message: Text("Slider value: \(roundedValue)"), dismissButton: .default(Text("Awesome")))
+                }
             }
         }
     }

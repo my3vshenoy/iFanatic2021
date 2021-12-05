08514a168846bb2f9b19e3a8143865375d9da8fb
My3 Shenoy <shenoymy31@gmail.com>
2021-12-05 13:50:00 -0500

    Added SF Symbols

---
+++ SwiftUIFundamentals/SwiftUIFundamentals/Views/RoundedViews.swift
@@ -0,0 +1,44 @@
+//
+//  RoundedViews.swift
+//  SwiftUIFundamentals
+//
+//  Created by My3 Shenoy on 12/5/21.
+//
+
+import SwiftUI
+
+struct RoundedImageViewStroked: View {
+    var systemName: String
+    var body: some View {
+        Image(systemName: systemName)
+            .font(.title)
+            .foregroundColor(Color("TextColor"))
+            .frame(width: 56, height: 56)
+    }
+}
+
+struct RoundedImageViewFilled: View {
+    var systemName: String
+    var body: some View {
+        Image(systemName: systemName)
+            .font(.title)
+            .foregroundColor(Color("TextColor"))
+            .frame(width: 56, height: 56)
+    }
+}
+
+struct PreviewView: View {
+    var body: some View {
+        VStack(spacing: 10) {
+            RoundedImageViewStroked(systemName: "arrow.counterclockwise")
+            RoundedImageViewFilled(systemName: "list.dash")
+        }
+    }
+}
+
+struct RoundedViews_Previews: PreviewProvider {
+    static var previews: some View {
+        PreviewView()
+        PreviewView().preferredColorScheme(.dark)
+    }
+}
